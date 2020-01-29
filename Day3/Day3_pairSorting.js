135ed709fc4db6a639c82f3a36dda4cfa06dd4c0
Marcus Beirne <icanseemyhousefromhere@hotmail.com>
2020-01-29 09:45:26 +0000

    Working solution. Iterates over sock array and populates the temp array with a sock. Counts the number of instances of the sock in the sock Array and increments the pair count by the number of paired instances of the sock. Checking the temp array each iteration means that we only count the pairs once per colour.

--- Day3/Day3_pairSorting.js
+++ Day3/Day3_pairSorting.js
@@ -1,6 +1,6 @@
 /*
 John works at a clothing store. He has a large pile of socks that he must pair by color for sale. Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
-For example, there are  socks with colors . There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+For example, there are 5 socks with colors . There is one pair of color 1 and one of color 2. There are one odd socks left, one of color 3. The number of pairs is 2.
 
 Function Description
 Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
@@ -30,22 +30,42 @@ Sample Output
 // Iterating through the array that would give us [1]; [1],[2]; [1],[2],[3]; [1,1],[2],[3]; [1,1],[2,2],[3];
 // We can then just sum the math.floor(length/2) of the secondary arrays
 // This works, but only when we know the number of different colours we are dealing with.
+x = [1,2,3,1,2];
+y = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+knownColours(x);
+unknownColours(y);
 
 // Known colours. Complexity O(n)
-var x = [1, 2, 3, 1, 2];
+function knownColours(sockAr) {
+    console.log("--- Known colours, solution 1 ---");
+    var array1=[], array2=[], array3=[];
 
-var array1=[], array2=[], array3=[];
+    for(var i=sockAr.length-1; i >= 0; i--)
+    {
+        var temp = sockAr.pop();
+        if (temp == 1) array1.push(temp);
+        else if (temp == 2) array2.push(temp);
+        else if (temp == 3) array3.push(temp);
+    }
 
-for(var i=x.length-1; i >= 0; i--)
-{
-    var temp = x.pop();
-    console.log("Temp: " + temp);
-    if (temp == 1) array1.push(temp);
-    else if (temp == 2) array2.push(temp);
-    else if (temp == 3) array3.push(temp);
+    var pairs = Math.floor(array1.length/2) + Math.floor(array2.length/2) + Math.floor(array3.length/2);
+    console.log("Number of pairs: " + pairs);
 }
 
-var pairs = Math.floor(array1.length/2) + Math.floor(array2.length/2) + Math.floor(array3.length/2);
-console.log("Number of pairs: " + pairs);
-
-
+// Solution adapted from 1st solution https://www.codementor.io/@rezehnde/sock-merchant-challenge-solved-in-c-10iz1059au
+function unknownColours(sockAr) {
+    console.log("--- Known colours, solution 2 ---");
+    var pairs = 0;
+    var hashSet = [];
+    sockAr.forEach(sock => {
+        if(!hashSet.includes(sock)) {
+            var matchedItems = sockAr.filter(match => match === sock);
+            var occurences = matchedItems.length;
+            if(occurences > 1) {
+                pairs += ((occurences - (occurences % 2)) / 2);
+            }
+            hashSet.push(sock);
+        }        
+    });
+    console.log("Number of pairs: " + pairs);
+}
